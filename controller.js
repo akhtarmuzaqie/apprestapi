f442e26ee3f7a11d663010b15d85046c85d75bd2
akhtarmuzaqie <akhtarabraar23@gmail.com>
2021-09-20 08:07:01 +0700

    Show data Mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,17 @@ var connection = require('./conn');
 exports.index = function (req, res) {
     response.ok("My REST API App Is Running", res);
 }
+
+
+//menampilkan semua data mahasiswa
+
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query(' SELECT * FROM mahasiswa', function (error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+};
+
